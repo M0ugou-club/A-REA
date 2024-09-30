27ae7b145e3cfd7dd2556e90b94fa48fb7641f4c
albe2 <alberic.flour-de-saegher@epitech.eu>
2024-09-30 22:20:08 +0200

    ADD : authToken gestion and redirection

--- front/web/src/app/loginPage/form/form.component.ts
+++ front/web/src/app/loginPage/form/form.component.ts
@@ -8,62 +8,129 @@ import {FormGroup} from "@angular/forms";
   styleUrl: './form.component.scss'
 })
 export class FormComponent implements OnInit{
-  isSignUp: boolean = false;
+  isRegister: boolean = false;
 
-  signupUsers: any[] = [];
+  RegisterUsers: any[] = [];
 
-  signupObj: any = {
+  RegisterObj: any = {
     email: '',
     username: '',
     password: ''
   };
 
   loginObj: any = {
-    username: '',
+    email: '',
     password: ''
   };
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    const localData = localStorage.getItem('signupUsers');
+    const localData = localStorage.getItem('authToken');
+
     if (localData != null) {
-      this.signupUsers = JSON.parse(localData);
+      fetch("http://localhost:8000/isLogged", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localData
+        }
+      })
+          .then(response => {
+            if (response.status === 200) {
+              this.router.navigate(['/']);
+            }
+          })
+          .catch((error) => {
+            console.error('Erreur de requête:', error);
+          });
     }
   }
 
-  onSignUp() {
+  onRegister() {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
-    if (!this.signupObj.email || !this.signupObj.username || !this.signupObj.password) {
+    if (!this.RegisterObj.email || !this.RegisterObj.username || !this.RegisterObj.password) {
       alert('Veuillez remplir tous les champs : email, nom d\'utilisateur et mot de passe.');
       return;
     }
 
-    if (emailRegex.test(this.signupObj.email)) {
-      this.signupUsers.push(this.signupObj);
-      localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
-      this.isSignUp = true;
-    } else {
+    if (!emailRegex.test(this.RegisterObj.email)) {
       alert('Veuillez entrer une adresse email valide.');
+      return;
     }
+
+    this.RegisterUsers.push(this.RegisterObj);
+
+    fetch("http://localhost:8000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.RegisterObj)
+    })
+        .then(response => {
+          if (response.status === 500) {
+            alert('Email invalid');
+          }
+          if (response.status === 201) {
+            console.log('Inscription réussie:');
+            this.isRegister = true;
+          }
+        })
+        .catch((error) => {
+          console.error('Erreur de requête:', error);
+        });
   }
 
   onLogin() {
-    if (!this.loginObj.username || !this.loginObj.password) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    if (!emailRegex.test(this.loginObj.email)) {
+      alert('Veuillez entrer une adresse email valide.');
+      return;
+    }
+
+    if (!this.loginObj.email || !this.loginObj.password) {
       alert('Veuillez remplir tous les champs : email, nom d\'utilisateur et mot de passe.');
       return;
     }
 
-    const isUserLogin = this.signupUsers.find(user => user.username === this.loginObj.username && user.password === this.loginObj.password)
-    if (isUserLogin != undefined) {
-      this.router.navigate(['/']);
-    } else {
-      alert("Wrong username or password");
-    }
+    console.log(JSON.stringify(this.loginObj));
+
+    fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.loginObj)
+    })
+        .then(response => {
+          if (response.status === 404) {
+            alert('Username invalid');
+            return null;
+          } else if (response.status === 401) {
+            alert('Password invalid');
+            return null;
+          } else if (response.status === 200) {
+            return response.json().then(data => {
+              console.log('Login réussie:');
+
+              const token = data.token;
+
+              localStorage.setItem('authToken', token);
+              this.router.navigate(['/']);
+            });
+          }
+          return null
+        })
+        .catch((error) => {
+          console.error('Erreur de réseau:', error);
+        });
   }
 
-  goToLogin(): void {
-    this.isSignUp = true;
+
+    goToLogin(): void {
+    this.isRegister = true;
   }
 }
