3fa60c700ce1eef39b514d0afcfd177ec60275ba
gpixel <matthias.soual@epitech.eu>
2024-10-02 14:56:16 +0200

    Revert "ADD: Display areas from array"
    This reverts commit 4c4b77c85cee61994cb91a36e1745760512346b4.

--- front/web/src/app/a-rea-page/a-rea-page.component.ts
+++ front/web/src/app/a-rea-page/a-rea-page.component.ts
@@ -6,37 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './a-rea-page.component.scss'
 })
 export class AREAPageComponent {
-  showAddArea = false;
 
-  addButton() {
-    this.showAddArea = !this.showAddArea;
-    console.log('showAddArea', this.showAddArea);
-  }
-
-  areas = [
-    {
-        actionLogo: 'https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png',
-        reactionLogo: 'https://cdn.worldvectorlogo.com/logos/discord-6.svg',
-        actionText: 'Spotify works!',
-        reactionText: 'Discord works too!'
-    },
-    {
-        actionLogo: 'https://img.icons8.com/?size=100&id=19318&format=png&color=000000',
-        reactionLogo: 'https://img.icons8.com/?size=100&id=118640&format=png&color=000000',
-        actionText: 'First icon action!',
-        reactionText: 'First icon reaction!'
-    },
-    {
-        actionLogo: 'https://cdn.iconscout.com/icon/free/png-256/twitter-6-1175160.png',
-        reactionLogo: 'https://cdn.iconscout.com/icon/free/png-256/facebook-3-1175156.png',
-        actionText: 'Twitter is fun!',
-        reactionText: 'Facebook connects us!'
-    },
-    {
-        actionLogo: 'https://cdn.iconscout.com/icon/free/png-256/youtube-3521665-2945200.png',
-        reactionLogo: 'https://cdn.iconscout.com/icon/free/png-256/instagram-3-1175111.png',
-        actionText: 'YouTube videos!',
-        reactionText: 'Instagram posts!'
-    },
-  ];
 }
