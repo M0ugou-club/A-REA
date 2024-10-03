2a35cc37189f871e1529249c1b735f193115ae7a
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-10-03 13:58:07 +0200

    EDIT: change style of navbar like figma mokeup

--- front/web/src/app/app.module.ts
+++ front/web/src/app/app.module.ts
@@ -12,11 +12,17 @@ import { HeaderComponent } from './header/header.component';
 import { LoginServicsePopupComponent } from './header/login-servicse-popup/login-servicse-popup.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, LoginPageComponent, FormComponent, HomePageComponent, HeaderComponent, LoginServicsePopupComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, BrowserAnimationsModule, FormsModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, BrowserAnimationsModule, FormsModule, HttpClientModule, FloatLabelModule, MatInputModule, MatFormFieldModule, MatButtonModule],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
