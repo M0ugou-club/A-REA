5eedb19c9972adc5589e8651b56fbd07908aeb35
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-10-08 00:59:37 +0200

    ADD : 2 openMeteo action

--- BACK/src/utils/actions_types.js
+++ BACK/src/utils/actions_types.js
@@ -4,6 +4,10 @@ const actions = {
     "on_new_playlist_spotify": "On new playlist",
     "on_like_track_spotify": "On user like new track"
   },
+  OpenMeteo: {
+    "on_evrest_melt": "When evrest temperature is higher than 0°C",
+    "on_evrest_almost_melting": "When evrest temperature is higher than -10°C"
+  },
   Discord: {
     "on_new_message_discord": "On new message",
     "on_new_reaction_discord": "On new reaction",
