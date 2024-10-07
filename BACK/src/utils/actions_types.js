7bf50d59e3fc8d24457c50299ae406fd8ccde2e1
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-10-07 22:41:14 +0200

    ADD : service for logic reaction and action

--- BACK/src/utils/actions_types.js
+++ BACK/src/utils/actions_types.js
@@ -1,7 +1,6 @@
 const actions = {
   Spotify: {
     "on_new_track_spotify": "On new track",
-    "on_new_album_spotify": "On new album",
     "on_new_playlist_spotify": "On new playlist",
     "on_like_track_spotify": "On user like new track"
   },
--- BACK/src/utils/areasTriggers.js
+++ BACK/src/utils/areasTriggers.js
@@ -1,6 +1,8 @@
 import User from '../../src/models/Users/index.js';
+import Actions from '../../src/models/Action/index.js';
 import actions from './actions_types.js'
 import { getAccesTokensServiceByUserId } from '../routes/tokens/indexService.js'
+import { reactionService } from './reactionService.js'
 
 export const actionsTriggers = async () => {
     const datasObject = {};
@@ -16,8 +18,8 @@ export const actionsTriggers = async () => {
     users.forEach(async user => {
         user.a_rea.forEach(async area => {
             console.log(user.username);
-            if (await actionsChanges(area.action.platform, area.action.type, area.action.data, user._id, area._id)) {
-                console.log('do reaction');
+            if (await actionsChanges(area.action.platform, area.action.type, area.action.data, user._id, area.action._id) === true) {
+                reactionService(area.reactions.platform, area.reactions.type, user.id);
             }
         });
     }); 
@@ -28,7 +30,7 @@ const actionsChanges = async (platform, type, data, userId, areaId) => {
     if (platform === 'Spotify') {
         const accessToken = await getAccesTokensServiceByUserId('Spotify', userId);
         console.log(accessToken);
-        if (await actionsTriggersSpotify(type, data, accessToken, userId, areaId)) {
+        if (await actionsTriggersSpotify(type, data, accessToken, areaId)) {
             return true;
         }
     }
@@ -38,28 +40,100 @@ const actionsChanges = async (platform, type, data, userId, areaId) => {
     return false;
 }
 
-const actionsTriggersSpotify = async (type, data, accessToken, userId, areaId) => {
+const actionsTriggersSpotify = async (type, data, accessToken, areaId) => {
     if (type === 'on_new_track_spotify') {
-        return true;
+        if (await spotifyActionsNewTrack(data, accessToken, areaId)) {
+            return true;
+        }
     }
     if (type === 'on_new_album_spotify') {
         console.log('on_new_album_spotify');
         return true;
     }
     if (type === 'on_new_playlist_spotify') {
-        console.log('on_new_playlist_spotify');
+        if (await spotifyPlaylistNew(data, accessToken, areaId))
         return true;
     }
     if (type === 'on_like_track_spotify') {
         console.log('on_like_track_spotify');
-        if (await spotifyActionsLikeTrack(data, accessToken, userId, areaId)) {
+        if (await spotifyActionsLikeTrack(data, accessToken, areaId)) {
             return true;
         }
     }
     return false;
 }
 
-const spotifyActionsLikeTrack = async (data, accessToken, userId, areaId) => {
+const spotifyPlaylistNew = async (data, accessToken, areaId) => {
+    try {
+        const response = await fetch("https://api.spotify.com/v1/me/playlists?limit=1", {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        if (response.ok) {
+            const result = await response.json();
+            const track = String(result.items[0].name);
+            console.log("track sa mere");
+            console.log(track);
+
+            console.log("areaid", areaId);
+            if (track != data || data === null) {
+                console.log('Music liked successfully!');
+                await Actions.findOneAndUpdate(
+                    { _id: areaId },
+                    { data: track }
+                )
+                return true;
+            }
+            return false;
+        } else {
+            console.log(response);
+            console.log("error");
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    return true;
+}
+
+const spotifyActionsNewTrack = async (data, accessToken, areaId) => {
+    try {
+        const response = await fetch("https://api.spotify.com/v1/artists/44BwcqsS9V20HWSeql39ah/albums?include_groups=single&limit=1&market=FR", {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        if (response.ok) {
+            const result = await response.json();
+            const track = String(result.items[0].name);
+            console.log("track");
+            console.log(track);
+
+            console.log("areaid", areaId);
+            if (track != data || data === null) {
+                console.log('Music liked successfully!');
+                await Actions.findOneAndUpdate(
+                    { _id: areaId },
+                    { data: track }
+                )
+                return true;
+            }
+            return false;
+        } else {
+            console.log(response);
+            console.log("error");
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    return true;
+}
+
+const spotifyActionsLikeTrack = async (data, accessToken, areaId) => {
     try {
         console.log('like track');
         const response = await fetch("https://api.spotify.com/v1/me/tracks?limit=1", {
@@ -70,14 +144,17 @@ const spotifyActionsLikeTrack = async (data, accessToken, userId, areaId) => {
         });
         if (response.ok) {
             const result = await response.json();
-            const track = result.items[0].track;
-            console.log(track.name);
+            const track = String(result.items[0].track.name);
             console.log('Music liked successfully!');
-            if (track.name === data || data === null) {
-                await User.updateOne(
-                    { _id: userId, 'a_rea._id': areaId },
-                    { $set: { 'a_rea.$.action.data': track.name } }
-                );
+            
+            console.log(track);
+            console.log(areaId);
+            if (track != data || data === null) {
+                console.log('Music liked successfully!');
+                await Actions.findOneAndUpdate(
+                    { _id: areaId },
+                    { data: track }
+                )
                 return true;
             }
             return false;
