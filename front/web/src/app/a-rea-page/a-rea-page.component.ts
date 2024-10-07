bdbf2d4922b45141798d0475dfccd2e79383d0b8
gpixel <matthias.soual@epitech.eu>
2024-10-07 17:56:56 +0200

    ADD: data fetch for areas

--- front/web/src/app/a-rea-page/a-rea-page.component.ts
+++ front/web/src/app/a-rea-page/a-rea-page.component.ts
@@ -4,91 +4,64 @@ import { Router } from "@angular/router";
 @Component({
   selector: "app-a-rea-page",
   templateUrl: "./a-rea-page.component.html",
-  styleUrl: "./a-rea-page.component.scss",
+  styleUrls: ["./a-rea-page.component.scss"],
 })
-
 export class AREAPageComponent {
   constructor(private router: Router) {}
+
+  private apiUrl = "http://localhost:8000/areas";
+  
+  areas: { actionLogo: string; reactionLogo: string; actionText: string; reactionText: string }[] = [];
 
   addButton() {
     this.router.navigateByUrl("add-a-rea");
   }
 
-  areas = [
-    {
-      actionLogo:
-        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
-      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
-      actionText: "Spotify works!",
-      reactionText: "Discord works too!",
-    },
-    {
-      actionLogo:
-        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
-      reactionLogo:
-        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
-      actionText: "First icon action!",
-      reactionText: "First icon reaction!",
-    },
-    {
-      actionLogo:
-        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
-      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
-      actionText: "Spotify works!",
-      reactionText: "Discord works too!",
-    },
-    {
-      actionLogo:
-        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
-      reactionLogo:
-        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
-      actionText: "First icon action!",
-      reactionText: "First icon reaction!",
-    },
-    {
-      actionLogo:
-        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
-      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
-      actionText: "Spotify works!",
-      reactionText: "Discord works too!",
-    },
-    {
-      actionLogo:
-        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
-      reactionLogo:
-        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
-      actionText: "First icon action!",
-      reactionText: "First icon reaction!",
-    },
-    {
-      actionLogo:
-        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
-      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
-      actionText: "Spotify works!",
-      reactionText: "Discord works too!",
-    },
-    {
-      actionLogo:
-        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
-      reactionLogo:
-        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
-      actionText: "First icon action!",
-      reactionText: "First icon reaction!",
-    },
-    {
-      actionLogo:
-        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
-      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
-      actionText: "Spotify works!",
-      reactionText: "Discord works too!",
-    },
-    {
-      actionLogo:
-        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
-      reactionLogo:
-        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
-      actionText: "First icon action!",
-      reactionText: "First icon reaction!",
-    },
-  ];
+  ngOnInit(): void {
+    const localData = localStorage.getItem("authToken");
+
+    if (localData != null) {
+      fetch("http://localhost:8000/isLogged", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localData,
+        },
+      })
+        .then((response) => {
+          if (response.status != 200) {
+            this.router.navigate(["/login"]);
+          }
+        })
+        .catch((error) => {
+          console.error("Request error:", error);
+        });
+    } else {
+      this.router.navigate(["/login"]);
+    }
+
+    fetch(this.apiUrl, {
+      method: "GET",
+      headers: {
+        authorization: "Bearer " + localStorage.getItem("authToken"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+    
+        data.forEach((area: any) => {
+          const reactionPlatform = area.reactions && area.reactions.length > 0 ? area.reactions[0].platform : 'No reaction';
+          this.areas.push({
+            actionLogo: 'https://cdn.worldvectorlogo.com/logos/discord-6.svg',
+            reactionLogo: 'https://cdn.worldvectorlogo.com/logos/discord-6.svg',
+            actionText: area.action.platform,
+            reactionText: reactionPlatform,
+          });
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching areas:", error);
+      });
+    }
 }
