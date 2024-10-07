d15d7a26d96fd6b0e1696c1a3e7cdffe7bc32702
gpixel <matthias.soual@epitech.eu>
2024-10-07 10:18:34 +0200

    ADD: areas display responsive

--- front/web/src/app/a-rea-page/a-rea-page.component.ts
+++ front/web/src/app/a-rea-page/a-rea-page.component.ts
@@ -30,5 +30,65 @@ export class AREAPageComponent {
       actionText: "First icon action!",
       reactionText: "First icon reaction!",
     },
+    {
+      actionLogo:
+        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
+      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
+      actionText: "Spotify works!",
+      reactionText: "Discord works too!",
+    },
+    {
+      actionLogo:
+        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
+      reactionLogo:
+        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
+      actionText: "First icon action!",
+      reactionText: "First icon reaction!",
+    },
+    {
+      actionLogo:
+        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
+      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
+      actionText: "Spotify works!",
+      reactionText: "Discord works too!",
+    },
+    {
+      actionLogo:
+        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
+      reactionLogo:
+        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
+      actionText: "First icon action!",
+      reactionText: "First icon reaction!",
+    },
+    {
+      actionLogo:
+        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
+      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
+      actionText: "Spotify works!",
+      reactionText: "Discord works too!",
+    },
+    {
+      actionLogo:
+        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
+      reactionLogo:
+        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
+      actionText: "First icon action!",
+      reactionText: "First icon reaction!",
+    },
+    {
+      actionLogo:
+        "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
+      reactionLogo: "https://cdn.worldvectorlogo.com/logos/discord-6.svg",
+      actionText: "Spotify works!",
+      reactionText: "Discord works too!",
+    },
+    {
+      actionLogo:
+        "https://img.icons8.com/?size=100&id=19318&format=png&color=000000",
+      reactionLogo:
+        "https://img.icons8.com/?size=100&id=118640&format=png&color=000000",
+      actionText: "First icon action!",
+      reactionText: "First icon reaction!",
+    },
   ];
 }
