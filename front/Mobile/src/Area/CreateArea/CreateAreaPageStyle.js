b9203878135f4978bbd54f2a29b8a310934ed00d
Louis Huguet <louis.huguet@epitech.eu>
2024-10-07 19:37:25 +0200

    ADD : Add area page not connectid with back-end

--- front/Mobile/src/Area/CreateArea/CreateAreaPageStyle.js
+++ front/Mobile/src/Area/CreateArea/CreateAreaPageStyle.js
@@ -10,38 +10,106 @@ export default StyleSheet.create({
 
     pageContainer: {
         flex: 1,
-        flexDirection: 'column',
         backgroundColor: 'white',
     },
 
-    actionContainer: {
-        height: height * 0.3,
+    actionContainerWrapper: {
+        height: height * 0.25,
         backgroundColor: '#88b4db',
         borderBottomRightRadius: width * 0.05,
         borderBottomLeftRadius: width * 0.05,
+        overflow: 'hidden',
     },
 
-    reactionContainer: {
-        height: height * 0.3,
+    actionContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: width * 0.02,
+        paddingRight: width * 0.02,
+        gap: width * 0.02,
+    },
+
+    reactionContainerWrapper: {
+        height: height * 0.25,
         backgroundColor: '#88b4db',
         borderTopRightRadius: width * 0.05,
         borderTopLeftRadius: width * 0.05,
+        overflow: 'hidden',
+    },
+
+    reactionContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: width * 0.02,
+        paddingRight: width * 0.02,
+        gap: width * 0.02,
     },
 
     areaContainer: {
         flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
 
     actionButton: {
         width: width * 0.3,
         height: width * 0.3,
-        backgroundColor: '#3f72af',
+        backgroundColor: '#5b94c7',
         justifyContent: 'center',
         alignItems: 'center',
+        borderRadius: width * 0.03,
+        marginRight: width * 0.02,
+    },
+
+    selectedActionButton: {
+        position: 'absolute',
+        top: height * 0.1,
+        width: width * 0.3,
+        left: width * 0.025,
+        height: width * 0.3,
+        backgroundColor: '#5b94c7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: width * 0.03,
+        marginRight: width * 0.02,
+    },
+
+    selectedReactionButton: {
+        position: 'absolute',
+        left: width * 0.675,
+        top: height * 0.1,
+        width: width * 0.3,
+        height: width * 0.3,
+        backgroundColor: '#5b94c7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: width * 0.03,
+        marginRight: width * 0.02,
+    },
+
+    textButton: {
+        fontSize: width * 0.05,
+        color: 'white',
+        fontFamily: 'Nexa',
+    },
+
+    arrowImage: {
+        position: 'absolute',
+        left: width * 0.35,
+        resizeMode: 'contain',
+        width: width * 0.3,
+    },
+
+    createArea: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: height * 0.325,
+        left: width * 0.2,
+        width: '60%',
+        height: '12.5%',
+        backgroundColor: '#3f72af',
         borderRadius: width * 0.03,
     },
 });
