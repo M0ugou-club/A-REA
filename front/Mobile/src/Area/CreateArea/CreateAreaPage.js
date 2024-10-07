1ad069e0562db838d520dc9da92655ef3c4f8857
Louis Huguet <louis.huguet@epitech.eu>
2024-10-07 18:18:41 +0200

    FIX : dl build apk mandatories dependencies

--- front/Mobile/src/Area/CreateArea/CreateAreaPage.js
+++ front/Mobile/src/Area/CreateArea/CreateAreaPage.js
@@ -3,10 +3,38 @@ import styles from './CreateAreaPageStyle';
 import NavigationBar from '../../NavigationBar/NavigationBar';
 
 export default function CreateAreaPage() {
+
+    const actionList = [
+        "Action 1",
+        "Action 2",
+        "Action 3",
+        "Action 4",
+        "Action 5",
+        "Action 6",
+    ]
+
+    const reactionList = [
+        "Reaction 1",
+        "Reaction 2",
+        "Reaction 3",
+        "Reaction 4",
+        "Reaction 5",
+    ]
+
     return (
         <View style={styles.globalContainer}>
-            <View>
-                
+            <View style={styles.pageContainer}>
+                <View style={styles.actionContainer}>
+                    {actionList.map((action, index) => {
+                        return (
+                            <TouchableOpacity key={index} style={styles.actionButton}>
+                                <Text style={{ fontSize: 20, color: 'white' }}>{action}</Text>
+                            </TouchableOpacity>
+                        );
+                    })}
+                </View>
+                <View style={styles.areaContainer}></View>
+                <View style={styles.reactionContainer}></View>
             </View>
             <NavigationBar />
         </View>
--- front/Mobile/src/Area/CreateArea/CreateAreaPageStyle.js
+++ front/Mobile/src/Area/CreateArea/CreateAreaPageStyle.js
@@ -7,4 +7,41 @@ export default StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-end'
     },
+
+    pageContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+    },
+
+    actionContainer: {
+        height: height * 0.3,
+        backgroundColor: '#88b4db',
+        borderBottomRightRadius: width * 0.05,
+        borderBottomLeftRadius: width * 0.05,
+    },
+
+    reactionContainer: {
+        height: height * 0.3,
+        backgroundColor: '#88b4db',
+        borderTopRightRadius: width * 0.05,
+        borderTopLeftRadius: width * 0.05,
+    },
+
+    areaContainer: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    actionButton: {
+        width: width * 0.3,
+        height: width * 0.3,
+        backgroundColor: '#3f72af',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: width * 0.03,
+    },
 });
