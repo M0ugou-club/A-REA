acb5a474267ce35e696179bf478d7afcda84de94
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-10-08 14:53:10 +0200

    DELETE : discord service from back

--- BACK/src/utils/reactions_types.js
+++ BACK/src/utils/reactions_types.js
@@ -4,12 +4,6 @@ const reactions = {
     "Pause": "Pause music",
     "AddQueue": "Add Mili mili to queue",
     "Like": "Like Mili Mili music"
-  },
-  Discord: {
-    "join_server": "Join the server",
-    "send_message": "Send a message",
-    "send_reaction": "Send a reaction",
-    "send_file": "Send a file"
   }
 }
 
