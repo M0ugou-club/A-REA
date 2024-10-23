654a7687932718f11237da39428fcd5904e93d97
albe2 <alberic.flour-de-saegher@epitech.eu>
2024-10-23 18:02:32 +0200

    DELETE: icons from /platforms

--- back/src/utils/platforms_icons.js
+++ back/src/utils/platforms_icons.js
@@ -1,37 +1,30 @@
 const platforms_icons = {
   "Spotify": {
-    "icon": "https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png",
     "color": "#429660",
     "connexion": true
   },
   "Deezer": {
-    "icon": "https://agreeable.fr/uploads/thumbnails/novo-logo-deezer-sem-fundo-66eaf4a6e0019274130722.png",
     "color": "#5064AC",
     "connexion": true
   },
   "OpenMeteo": {
-    "icon": "https://open-meteo.com/favicon-32x32.png",
     "color": "#D48207",
     "connexion": false
   },
   "X": {
-    "icon": "https://upload.wikimedia.org/wikipedia/commons/1/19/X_logo_twitter_new_brand_icon.svg",
-    "color": "#1DA1F2", 
+    "color": "#1DA1F2",
     "connexion": true
   },
   "Reddit": {
-    "icon": "https://upload.wikimedia.org/wikipedia/commons/b/b4/Reddit_logo.svg",
     "color": "#FF4500",
     "connexion": true
   },
   "Youtube": {
-    "icon": "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg",
     "color": "#9F3531",
     "connexion": true
   },
   "Discord": {
-    "icon": "https://img.icons8.com/color/512/discord-logo.png",
-    "color": "#E1306C",
+    "color": "#5064AC",
     "connexion": true
   },
 };
