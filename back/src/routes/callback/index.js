f873dfdcbac2cac40a526096339689dd230132ce
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-11-03 18:58:56 +0100

    ADD: .env var for url frontend

--- back/src/routes/callback/index.js
+++ back/src/routes/callback/index.js
@@ -13,19 +13,19 @@ callbackRoutes.get("/callback/:service", async (req, res) => {
   switch (req.params.service) {
     case "Spotify":
       spotifyCallbackService(req, res);
-      return res.redirect("http://localhost:4200/dashboard");
+      return res.redirect(process.env.FRONTEND_URL + "/dashboard");
     case "Youtube":
       youtubeCallbackService(req, res);
-      return res.redirect("http://localhost:4200/dashboard");
+      return res.redirect(process.env.FRONTEND_URL + "/dashboard");
     case "Twitch":
       twitchCallbackService(req, res);
-      return res.redirect("http://localhost:4200/dashboard");
+      return res.redirect(process.env.FRONTEND_URL + "/dashboard");
     case "X":
       xCallbackService(req, res);
-      return res.redirect("http://localhost:4200/dashboard");
+      return res.redirect(process.env.FRONTEND_URL + "/dashboard");
     case "Reddit":
       redditCallbackService(req, res);
-      return res.redirect("http://localhost:4200/dashboard");
+      return res.redirect(process.env.FRONTEND_URL + "/dashboard");;
     default:
       return res.status(404).send({ message: "Service not found" });
   }
